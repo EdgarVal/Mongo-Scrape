38757914273ee8441aa19194da048ea73eb5dd23
edgar <GrrrRocks@gmail.com>
2019-03-16 14:27:09 -0700

    fixed heroku port

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var logger = require("morgan");
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoScrape";
 
 mongoose.connect(MONGODB_URI);
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
